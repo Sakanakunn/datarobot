e70c14989330c8b79c930309b0d4ee46ebffdbe8
osanai_masahiro <osanai_masahiro@osanai-masahironoMacBook.local>
2018-03-18 16:17:31 +0900

    Finished convert csv to pandas.DataFrame!

--- route.py
+++ route.py
@@ -13,12 +13,23 @@ def greet(name='Stranger'):
     # Get the data
     csv_str = str(request.POST['upload'].file.read(),'utf-8')
     array_str = csv_str.split('\r\n')
-    array_fin = []
+    array_pre = []
     for i in array_str:
-        array_fin.append(i.split(','))
+        array_pre.append(i.split(','))
+
+    ## todo Python ArrayをPandas.DataFrameに変換する
+
+    ## todo Pythonの配列を組み替える
+    array_fin_dic = {}
+    for idx1,i in enumerate(array_pre[0]):
+        values = []
+        for idx2,t in enumerate(array_pre):
+            if idx2 != 0:
+                values.append(t[idx1])
+        array_fin_dic[str(i)] = values
 
     pdb.set_trace()
-    df = pd.DataFrame(array_str).to_html()
+    df = pd.DataFrame.from_dict(array_fin_dic).to_html()
     return template(df,name=name,pd=df)
 
 @get('/index')
